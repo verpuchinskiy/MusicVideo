faefb02538f82af3cb3f95fe19c5c10f6eb675d8
Mike Verpuchinskiy <mike@MikeBook.local>
2017-01-15 14:46:36 +0200

    Completed Part-28

--- MusicVideo/SettingsTVC.swift
+++ MusicVideo/SettingsTVC.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class SettingsTVC: UITableViewController {
+class SettingsTVC: UITableViewController, MFMailComposeViewControllerDelegate {
     
     @IBOutlet weak var aboutDisplay: UILabel!
     @IBOutlet weak var feedbackDisplay: UILabel!
@@ -65,6 +66,38 @@ class SettingsTVC: UITableViewController {
         bestImageDisplay.font = UIFont.preferredFont(forTextStyle: .subheadline)
         APICount.font = UIFont.preferredFont(forTextStyle: .subheadline)
     }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 1 {
+            let mailComposeViewController = configureMail()
+            
+            if MFMailComposeViewController.canSendMail() {
+                self.present(mailComposeViewController, animated: true, completion: nil)
+            } else {
+                mailAlert()
+            }
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
+    func configureMail() -> MFMailComposeViewController {
+        let mailComposeVC = MFMailComposeViewController()
+        mailComposeVC.mailComposeDelegate = self
+        mailComposeVC.setToRecipients(["mike.verpuchinskiy@gmail.com"])
+        mailComposeVC.setSubject("Music Video App Feedback")
+        mailComposeVC.setMessageBody("Hi Mike, \n\nI Would like to share the following feedback...\n", isHTML: false)
+        return mailComposeVC
+    }
+    
+    func mailAlert() {
+        let alertController = UIAlertController(title: "Alert", message: "No e-mail Account set up for Phone", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            
+        }))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 
     deinit {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: UIContentSizeCategoryNewValueKey), object: nil)
